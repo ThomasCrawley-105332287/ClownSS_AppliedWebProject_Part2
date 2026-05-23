05454bf3d08e2a3ffeac65abbef536b50bd20a23
106016142 <106016142@student.swin.edu.au>
2026-05-23 22:16:39 +1000

    Added database connection to login.php

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+require_once("../includes/settings.php");
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
 
 <!DOCTYPE html>

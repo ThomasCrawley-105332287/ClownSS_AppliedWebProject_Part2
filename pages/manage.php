81d24e11e6b6bd02b382deb0a45c717d3ac213a2
106016142 <106016142@student.swin.edu.au>
2026-05-23 23:20:55 +1000

    feat(manage): create base manage.php page structure

---
+++ pages/manage.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Manage EOIs</title>
+
+    <link rel="stylesheet" href="../styles/main_style_sheet.css">
+</head>
+
+<body>
+
+<?php include '../includes/header.inc'; ?>
+<?php include '../includes/nav.inc'; ?>
+
+<div class="nav_space"></div>
+
+<main>
+
+    <h1>Manage EOIs</h1>
+
+</main>
+
+<?php include '../includes/footer.inc'; ?>
+
+</body>
+</html>

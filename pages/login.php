45734268e1f86a8efb875f783706477c2c822fcc
106016142 <106016142@student.swin.edu.au>
2026-05-23 22:14:32 +1000

    Created base login.php page with includes and session support

---
+++ pages/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Manager Login</title>
+
+    <link rel="stylesheet" href="../styles/main_style_sheet.css">
+</head>
+
+<body>
+
+<?php include '../includes/header.inc'; ?>
+<?php include '../includes/nav.inc'; ?>
+
+<div class="nav_space"></div>
+
+<main>
+
+    <h1>Manager Login</h1>
+
+</main>
+
+<?php include '../includes/footer.inc'; ?>
+
+</body>
+</html>

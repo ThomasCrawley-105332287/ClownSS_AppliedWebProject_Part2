652f863665c24a5aa2f078e7061fe1834954a32b
106016142 <106016142@student.swin.edu.au>
2026-05-23 23:25:48 +1000

    feat(db): add database connection to manage.php

--- pages/manage.php
+++ pages/manage.php
@@ -10,6 +10,23 @@ if (!isset($_SESSION["username"])) {
 
     exit();
 }
+
+require_once("../includes/settings.php");
+
+$conn = mysqli_connect(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if (!$conn) {
+
+    die(
+        "Connection failed: "
+        . mysqli_connect_error()
+    );
+}
 ?>
 
 <!DOCTYPE html>
